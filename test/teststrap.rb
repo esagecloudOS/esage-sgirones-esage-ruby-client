52f132105bb13b349c72d8b29335f7a8e4401f19
杨晨 <1132431775@qq.com>
2018-12-28 13:49:23 +0800

    Ruby
    Ruby

---
+++ test/teststrap.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'riot'
+require File.expand_path(File.dirname(__FILE__)+"/../lib/abiquo_ruby_client.rb")
